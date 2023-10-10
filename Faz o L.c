cb28887d32290e63b6a0ba55051e7ee969602e91
Rafael7James <141595662+Rafael7James@users.noreply.github.com>
2023-10-10 11:32:10 -0300

    Add files via upload

---
+++ Faz o L.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int codigo, prato;
+float valor;
+
+printf("-------------- MENU --------------\n");
+   printf("1 | Picanha        | R$ 25,00\n");
+   printf("2 | Lasanha        | R$ 20,00\n");
+   printf("3 | Strogonoff     | R$ 18,00\n");
+   printf("4 | Bife acebolado | R$ 15,00\n");
+   printf("5 | Pao com ovo    | R$ 5,00\n");
+   printf("------------- FIM ---------------\n");
+   printf("Digite o codigo da opcao desejada\n");
+   scanf("%d", codigo);
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
