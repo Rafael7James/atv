b2f19018ab1f4f99571bcb7db8d40b2ba681d2fe
Rafael7James <141595662+Rafael7James@users.noreply.github.com>
2023-12-05 11:32:09 -0300

    Update Q2 VOID.C

--- Q2 VOID.C
+++ Q2 VOID.C
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
-void interface(){
- printf("\n==========================================\n");
+void interface()
+{
+    printf("\n==========================================\n");
     printf("\tTabuada\n");
     printf("\n==========================================\n");
 }
 
-void tabuada(int n1){
-int i;
-for ( i = 1; i < 10; i++)
+void tabuada(int n1)
 {
-printf("%d x %d = %d \n", n1, i, n1 * i);
+    int i;
+    for (i = 1; i < 10; i++)
+    {
+        printf("%d x %d = %d \n", n1, i, n1 * i);
+    }
 }
+int main()
+{
 
+    int numero;
 
+    interface();
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    interface();
+    tabuada(numero);
+
+    return 0;
 }
-int main(){
-
-
-
