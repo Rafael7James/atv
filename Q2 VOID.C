d88bb2be7f0b86a4b49769842390b5663bced2f9
Rafael7James <141595662+Rafael7James@users.noreply.github.com>
2023-12-05 11:27:27 -0300

    Update Q2 VOID.C

--- Q2 VOID.C
+++ Q2 VOID.C
@@ -6,23 +6,16 @@ void interface(){
     printf("\n==========================================\n");
 }
 
+void tabuada(int n1){
+int i;
+for ( i = 1; i < 10; i++)
+{
+printf("%d x %d = %d \n", n1, i, n1 * i);
+}
+
+
+}
 int main(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
-}
